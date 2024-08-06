b73eef4d9df3694df9621cf53dd20db0aaa5bf9d
Vlatko Stojkoski <stojvlatko@gmail.com>
2024-08-06 16:40:16 +0200

    fix padding logic based on rank

--- src/app/leaderboard/leaderboard-top-scores.tsx
+++ src/app/leaderboard/leaderboard-top-scores.tsx
@@ -30,13 +30,13 @@ function Ranking({ rank, name, score, isHighlighted }: { rank: number; name: str
 		<div
 			className={twMerge(
 				"w-full bg-background border shadow-lg rounded-lg col-span-full grid grid-cols-subgrid place-items-center items-center justify-center",
-				rank < 3 ? "py-3" : "",
+				rank <= 3 ? "py-3" : "",
 				isHighlighted ? "drop-shadow-glow -z-10 bg-primary-900" : ""
 			)}
 		>
 			<span className={`
                 p-3 size-[4ch] rounded-full leading-none bg-gradient-to-r
-                ${rank < 3 ? "font-bold text-center" : ""}
+                ${rank <= 3 ? "font-bold text-center" : ""}
                 ${rank === 1 ? "bg-gradient-to-br from-yellow-400 to-yellow-600" : ''}
                 ${rank === 2 ? "bg-gradient-to-br from-gray-400 to-gray-600" : ''}
                 ${rank === 3 ? "bg-gradient-to-br from-yellow-700 to-yellow-900" : ''}
